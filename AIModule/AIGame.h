ed3bd270e7bce8ee1c48b10515258fd91e587e2e
rogor <thomas.mainguy@gmail.com>
2014-05-03 18:41:00 +0200

    creation aimodule

---
+++ AIModule/AIGame.h
@@ -0,0 +1,21 @@
+/**
+ * Classe AIGame.
+ */
+
+#ifndef _AIGAME_H
+#define _AIGAME_H
+
+#include <vector>
+#include <stdexcept>
+#include "../GameModule/Card.h"
+#include "../GameModule/Bank.h"
+#include "../GameModule/Player.h"
+#include "../CommunicationModule/PlayerCommunication.h"
+#include "../InterfaceModule/AI.h"
+
+class AIGame
+{
+
+};
+
+#endif
