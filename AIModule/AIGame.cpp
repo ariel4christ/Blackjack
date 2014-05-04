f3949e7a1931eacb46e8d6e68c3ebd4f3cc69ef3
ariel4christ <nono.ariel@yahoo.it>
2014-05-04 18:38:51 +0200

    debut implementation IA

--- AIModule/AIGame.cpp
+++ AIModule/AIGame.cpp
@@ -1,2 +1,27 @@
 #include "AIGame.h"
 
+AIGame::AIGame():listOfCards(NULL),previousBets(NULL),players(NULL)
+{
+}
+
+void AIGaAIGame::~AIGame()
+{
+}
+
+void AIGame::runGame()
+{
+}
+
+void AIGame::runRound()
+{
+}
+
+void AIGame::chooseAction()
+{
+}
+
+void AIGame::quitGame()
+{
+}
+
+
--- AIModule/AIGame.h
+++ AIModule/AIGame.h
@@ -15,6 +15,23 @@
 
 class AIGame
 {
+public:
+
+	AIGame();
+	virtual ~AIGame();
+	void runGame();
+
+private:
+
+	std::vector<int> previousBets;
+	std::vector<Card> listOfCards;
+	std::vector<Player> players;
+	PlayerCommunication com;
+	AI aiInterface;
+
+	void quitGame();
+	void runRound();
+	void chooseAction();
 
 };
 
