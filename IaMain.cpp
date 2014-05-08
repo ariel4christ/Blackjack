fd9a7f110890c9174c2fd76cbc4735ce64757f17
ariel4christ <nono.ariel@yahoo.it>
2014-05-08 23:36:05 +0200

    ajout d'element de calcul de l'ia

---
+++ IaMain.cpp
@@ -0,0 +1,17 @@
+/*
+ * IaMain.cpp
+ *
+ *  Created on: 8 mai 2014
+ *      Author: ariel
+ */
+
+#include "AIModule/AIGame.h"
+
+int main(int argc, char const *argv[])
+{
+    AIGame ia;
+
+    ia.runGame();
+
+    return 0;
+}
